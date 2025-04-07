6f3affcd2bbb08ce96f5d8baa105bd060be60032
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2025-04-07 21:09:09 +0530

    style: refactor some styles

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,8 +3,8 @@ import { Link } from "react-router";
 const Home = () => {
 	return (
 		<article className="w-full min-h-screen text-center flex flex-col items-center justify-center px-8 bg-cover bg-no-repeat">
-			<h1 className="text-2xl font-bold">Welcome to the most trusted event list app</h1>
-			<p className="max-w-96 text-gray-900 text-xl mt-4">
+			<h1 className="text-gray-800 text-2xl font-bold">Welcome to the most trusted event list app</h1>
+			<p className="max-w-96 text-gray-600 text-lg font-semibold mt-4">
 				Connecting people of all faiths through events and community support
 			</p>
 			<Link to="event" className="bg-green-500 text-white px-4 py-2 rounded-md font-semibold mt-6">
--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,7 @@
 const NotFound = () => {
   return (
     <article className="min-h-screen flex justify-center items-center">
-      <p className="text-red-600 text-2xl font-bold">404 - PAGE NOT FOUND!</p>
+      <p className="text-gray-600 text-2xl font-bold text-center">404 - PAGE NOT FOUND!</p>
     </article>
   )
 }
